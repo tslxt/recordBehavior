7dedd23bca147c4d2fbefe891210e6246fa89974
tslxt <15460854@qq.com>
2015-11-05 08:20:35 +0800

    first version for test

--- app/servers/chat/filter/recordFilter.js
+++ app/servers/chat/filter/recordFilter.js
@@ -11,17 +11,34 @@ var fs = require('fs');
 Filter.prototype.before = function(msg, session, next) {
   if (msg.content) {
     var msgObj = JSON.parse(msg.content);
-    var recordPath = this.app.get('record').recordPath;
+    var recordPath = this.app.get('record').recordPath + '/' + msg.rid;
     if (!fs.exists(recordPath)) {
       fs.mkdir(recordPath, 0777, function(err) {
         
       });  
     }
-    if (msgObj.type === 'HANDWRITING') {
-      fs.appendFile(recordPath + '/' + msg.rid + '.record', JSON.stringify(msgObj.graphy) + '\r\n', function(err) {
-        if (err) throw err;
-      });
+    var ctime = new Date();
+    switch (msgObj.type) {
+      case 'HANDWRITING':
+        var suffix = '';
+        if ( msgObj.drawboardmode === 'courseWare' ) {
+            suffix = 'page' + '-' + String(msgObj.index);
+        }
+        if ( msgObj.drawboardmode === 'exercise' ) {
+            suffix = 'ex' + '-' + String(msgObj.index);
+        }
+        fs.appendFile(recordPath + '/' + msg.rid  + '.' + suffix, String(ctime.getTime()) + '_' + JSON.stringify(msgObj.graphy) + '\r\n', function(err) {
+          if (err) throw err;
+        });
+        break;
+      case 'PAGE':
+      case 'EXERCISE':
+      case 'OPERATION':
+        fs.appendFile(recordPath + '/' + msg.rid  + '.act' , String(ctime.getTime()) + '_' + JSON.stringify(msgObj) + '\r\n', function(err) {
+        });
+        break;
     }
+    
   }
 
   next();
